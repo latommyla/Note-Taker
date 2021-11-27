6c5f886665779a10042f1ed8889075a79847c0d9
Tommy La <tommyl.dmd@gmail.com>
2021-11-26 18:37:24 -0800

    testing deployment on heroku

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
-const express = require('express');
-
-const app = express();
-
 const PORT = process.env.PORT || 3001;
+const express = require('express');
+const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
